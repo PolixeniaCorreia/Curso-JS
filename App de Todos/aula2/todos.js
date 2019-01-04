8e5a3969e6af4b18e02213a491c3632238e330f2
“PolixeniaCorreia” <polixeniacorreia@gmail.com>
2019-01-03 22:10:29 -0300

    Iniciando aplicação

---
+++ App de Todos/aula2/todos.js
@@ -0,0 +1,11 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = [
+ 'Fazer café',
+ 'Estudar Javascript',
+ 'Acessar comunidade'
+// Se fosse um objeto
+// {text: 'Fazer café', propriedade: 1},
+];
