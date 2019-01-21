ef8f4d4e8d761197e8651d54f0db9baafdb1ee46
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-01-21 20:32:15 -0300

    Desafio 2 : Exibindo Repositório do Git em tempo de execução

---
+++ Desafio3/exercicio2/main.js
@@ -0,0 +1,25 @@
+var listElement = document.querySelector('ul');
+var inputElement = document.querySelector('input');
+
+function listar(){
+    var user = inputElement.value;
+
+    if(!user) return;
+
+    axios.get('https://api.github.com/users/' + user + '/repos')
+        .then(function (response) {
+            renderRepo(response.data);
+        })
+}
+
+function renderRepo(repositories){
+    for (repo of repositories){
+        var textElement = document.createTextNode(repo.name);
+        var liElement = document.createElement('li');
+
+        liElement.appendChild(textElement);
+        listElement.appendChild(liElement);
+    }
+}
+
+// PolixeniaCorreia
